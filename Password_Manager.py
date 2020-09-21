7240f9b708962a9724e7964fed25425378600c1b
Hashy-0x43 <Daemon84@protonmail.ch>
2020-09-21 00:07:57 -0400

    Create Password_Manager.py

---
+++ Password_Manager.py
@@ -0,0 +1,13 @@
+master_password = "password"
+
+discord_password = "password"
+discord_pin = 12345
+
+input_master_password = input("Master Password: ")
+
+if input_master_password == msater_password:
+  print("Discord = 12345")
+  pin = input("PIN: ")
+  
+  if pin == discord_pin:
+    print(discord_password)
